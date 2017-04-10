SSLMate/caa_helper

commit 63474e9c52ca486edd944f4b70a3d0ead52ef401

js: drop iodef array abstraction

script.js
@@ -19,17 +19,6 @@ function init_caa_helper (form, output, output_zonefile, output_rfc3597, output_
 		}
 		return items;
 	}
-	function get_iodef_array () {
-		var value = form["iodef"].value;
-		if (value != "") {
-			if (value.indexOf("mailto:") == 0 || value.indexOf("http:") == 0 || value.indexOf("https:") == 0) {
-				return [ value ];
-			} else {
-				return [ "mailto:" + value ];
-			}
-		}
-		return [];
-	}
 	function array_equals (a, b) {
 		if (a.length != b.length) {
 			return false;
@@ -126,14 +115,14 @@ function init_caa_helper (form, output, output_zonefile, output_rfc3597, output_
 					}
 				}
 			}
-			for (i = 0; i < this.iodef.length; ++i) {
-				records.push(new Record(0, "iodef", this.iodef[i]));
+			if (this.iodef != "") {
+				records.push(new Record(0, "iodef", this.iodef));
 			}
 			return records;
 		};
 	}
 	function make_policy_from_form () {
-		return new Policy(aggregate("issue"), aggregate("issuewild"), get_iodef_array());
+		return new Policy(aggregate("issue"), aggregate("issuewild"), form["iodef"].value);
 	}
 	function set_output (output, elts) {
 		while (output.hasChildNodes()) {

commit 64434d313ca2dea6451f88180ed6fcf44efa5219

Prepend iodef with mailto: if no protocol scheme present
Regression introduced in 63474e9c52ca486edd944f4b70a3d0ead52ef401.

Fixes #27

script.js
@@ -91,6 +91,20 @@ function init_caa_helper (form, output, output_zonefile, output_rfc3597, output_
 		var value = decode_string(bytes.slice(2 + tag_len));
 		return new Record(flags, tag, value);
 	}
+	function iodef_from_form (value) {
+		if (value == "" || value.indexOf("mailto:") == 0 || value.indexOf("http:") == 0 || value.indexOf("https:") == 0) {
+			return value;
+		} else {
+			return "mailto:" + value;
+		}
+	}
+	function iodef_to_form (value) {
+		if (value.indexOf("mailto:") == 0) {
+			return value.substr(7);
+		} else {
+			return value;
+		}
+	}
 	function Policy (issue, issuewild, iodef) {
 		this.issue = issue;
 		this.issuewild = issuewild;
@@ -131,11 +145,11 @@ function init_caa_helper (form, output, output_zonefile, output_rfc3597, output_
 			}
 			set_checkboxes("issue", this.issue);
 			set_checkboxes("issuewild", this.issuewild);
-			form["iodef"].value = this.iodef;
+			form["iodef"].value = iodef_to_form(this.iodef);
 		};
 	}
 	function make_policy_from_form () {
-		return new Policy(aggregate("issue"), aggregate("issuewild"), form["iodef"].value);
+		return new Policy(aggregate("issue"), aggregate("issuewild"), iodef_from_form(form["iodef"].value));
 	}
 	function InvalidRecordError (message) {
 		this.message = message;